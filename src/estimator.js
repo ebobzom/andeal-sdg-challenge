f62ed41af11faf21661c2442deaf72794c6c8093
Elijah Bobzom <ebobzom@gmail.com>
2020-04-08 12:58:17 +0100

    initial commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,32 @@
-const covid19ImpactEstimator = (data) => data;
+/* eslint linebreak-style: off */
+function normalizePeriod(periodType, timeToElapse) {
+  if (periodType.toLowerCase() === 'days') return timeToElapse;
+
+  if (periodType.toLowerCase() === 'weeks') return parseInt(timeToElapse * 7, 10);
+
+  if (periodType.toLowerCase() === 'months') return parseInt(timeToElapse * 30, 10);
+
+  if (periodType.toLowerCase() === 'years') return parseInt(timeToElapse * 30 * 365, 10);
+
+  return null;
+}
+
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+  const normalizedTime = normalizePeriod(data.periodType, data.timeToElapse);
+  const power = parseInt(normalizedTime / 3, 10);
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** power);
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** power);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
