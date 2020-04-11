c7adc55596a9bb16e6250e8a9f3b6034202fef64
Elijah Bobzom <ebobzom@gmail.com>
2020-04-11 07:56:34 +0100

    add backend link

--- src/estimator.js
+++ src/estimator.js
@@ -1,47 +1,5 @@
 /* eslint linebreak-style: off */
 
-// const data1 = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 6,
-//     avgDailyIncomePopulation: 0.55
-//   },
-//   reportedCases: 2883,
-//   population: 103463004,
-//   totalHospitalBeds: 2272236,
-//   timeToElapse: 27,
-//   periodType: 'days'
-// };
-
-// const data2 = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 6,
-//     avgDailyIncomePopulation: 0.72
-//   },
-//   reportedCases: 1382,
-//   population: 88712629,
-//   totalHospitalBeds: 1725402,
-//   timeToElapse: 1,
-//   periodType: 'weeks'
-// };
-
-// const data3 = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 3,
-//     avgDailyIncomePopulation: 0.57
-//   },
-//   reportedCases: 1790,
-//   population: 156590279,
-//   totalHospitalBeds: 2487868,
-//   timeToElapse: 5,
-//   periodType: 'months'
-// };
-
 function normalizePeriod(periodType, timeToElapse) {
   if (periodType.toLowerCase() === 'days') return timeToElapse;
 
@@ -61,7 +19,6 @@ function hospitalBedsAvailable(totalHospitalBeds, severeCasesByRequestedTime) {
 
 const covid19ImpactEstimator = (data) => {
   /* eslint no-console:off */
-  // console.log(data);
   const impact = {};
   const severeImpact = {};
   const normalizedTime = normalizePeriod(data.periodType, data.timeToElapse);
@@ -100,9 +57,5 @@ const covid19ImpactEstimator = (data) => {
     severeImpact
   };
 };
-
-// console.log(covid19ImpactEstimator(data1));
-// console.log(covid19ImpactEstimator(data2));
-// console.log(covid19ImpactEstimator(data3));
 
 export default covid19ImpactEstimator;
